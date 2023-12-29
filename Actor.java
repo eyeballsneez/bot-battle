e8ee490ae3b978589c841e35977844cd6241bd4e
salvadorio <38008394+salvadorio@users.noreply.github.com>
2023-12-29 00:09:57 -0800

    more stfuff
    wah!

--- Actor.java
+++ Actor.java
@@ -1,5 +1,14 @@
+import java.lang.Math;
+
 public class actor {
-    public int getY(){
+
+    int x;
+    int y;
+
+    public int getX() {
+        return x;
+    }
+    public int getY() {
         return y;
     }
 
@@ -9,15 +18,36 @@ public class actor {
 }
 public class base extends actor {
     private int money;
+    private int income;
 
-    public tick()
+    public base(int startingMoney, int startingIncome){
+        money = startingMoney;
+        income = startingIncome;
+    }
+
+    public tick() {
+        money += income;
+    }
+
 }
 
 public class guy extends actor {
     private double direction;
     private int speed;
+    private int maxSpeed;
 
     public tick() {
-        this.x += 
+        this.x += Math.sin(direction) * speed;
+        this.y += Math.cos(direction) * speed;
+    }
+
+    public void setDirection(double direction) {
+        this.direction = direction;
+    }
+
+    public void setSpeed(int speed) {
+        if (Math.abs(speed) < maxSpeed) {
+            this.speed = speed;
+        }
     }
 }
