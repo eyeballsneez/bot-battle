cac47fb3b779a459b8b97887f5266ea6636497f8
salvadorio <38008394+salvadorio@users.noreply.github.com>
2023-12-28 23:56:03 -0800

    Update Actor.java

--- Actor.java
+++ Actor.java
@@ -1,11 +1,23 @@
-public class Actor {
-    private int x;
-    private int y;
-
-    public int getX(){
-        return x;
-    }
+public class actor {
     public int getY(){
         return y;
+    }
+
+    public tick();
+
+    public draw();
+}
+public class base extends actor {
+    private int money;
+
+    public tick()
+}
+
+public class guy extends actor {
+    private double direction;
+    private int speed;
+
+    public tick() {
+        this.x += 
     }
 }
