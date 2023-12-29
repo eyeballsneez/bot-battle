058a86a7ed7cd3967c4db2bb1ec2db352183d416
salvadorio <38008394+salvadorio@users.noreply.github.com>
2023-12-29 00:32:37 -0800

    Update base.java

--- base.java
+++ base.java
@@ -2,9 +2,11 @@ public class base extends actor {
     private int money;
     private int income;
 
-    public base(int startingMoney, int startingIncome) {
+    public base(int startingMoney, int startingIncome, int xpos, int ypos) {
         money = startingMoney;
         income = startingIncome;
+        x = xpos;
+        y = ypos;
     }
 
     public void tick() {
