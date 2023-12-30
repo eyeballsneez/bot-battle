d5a066247e291a2fff03a4cf465c954ef953c1cb
phloamy <njshie@gmail.com>
2023-12-29 23:15:28 -0800

    DroneBot tick called from main

--- Main.java
+++ Main.java
@@ -13,15 +13,13 @@ public class Main {
         boolean gameRunning = true;
         Game game = new Game();
 
-        base base1 = new base(50, 1, 700, 700, game.team1Actors, game.team2Actors, game);
-        game.team1Actors.add(base1);
-        base1.spawnInfantryGuy();
+        base base1 = new base(50, 1, 600, 600, game.team1Actors, game.team2Actors, game);
+        game.add(base1, game.team1Actors);
+        //base1.spawnInfantryGuy();
 
-        base base2 = new base(50, 1, 100, 100, game.team2Actors, game.team1Actors, game);
+        base base2 = new base(50, 1, 300, 300, game.team2Actors, game.team1Actors, game);
         game.team2Actors.add(base2);
         base2.spawnInfantryGuy();
-        game.team2Actors.get(1).x = 250;
-        game.team2Actors.get(1).y = 300;
 
         GameView gameView = null;
 
@@ -45,6 +43,7 @@ public class Main {
         } else {
             while (gameRunning) {
                 game.tick();
+                team1Bot.tick();
 
                 gameView.redraw();
 
@@ -52,10 +51,25 @@ public class Main {
                 updateTime = System.nanoTime() - now;
                 wait = (OPTIMAL_TIME - updateTime) / 1000000;
 
+                if (game.team2Actors.get(0) instanceof base) {
+                    base base = (base) game.team2Actors.get(0);
+
+                    if (game.team2Actors.size() < 4) base.spawnInfantryGuy();
+                }
+
+                for (actor actor : game.team2Actors) {
+                    if (!(actor instanceof guy)) continue;
+                    infantryGuy infantry = (infantryGuy) actor;
+                    infantry.setSpeed(4);
+                    infantry.setDirection(infantry.getDirection() + (Math.random() - 0.5) * Math.PI);
+                }
+
+                /*
                 infantryGuy infantry = (infantryGuy) game.team1Actors.get(1);
                 infantry.setSpeed(4);
                 infantry.setDirection((double) LocalDateTime.now().getNano() / 1000000000 * 2 * Math.PI);
                 infantry.shoot();
+                 */
 
                 try {
                     Thread.sleep(wait);
