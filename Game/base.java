3b56257d51af5238f2d681ee2e0fd5612b4904ce
salvadorio <38008394+salvadorio@users.noreply.github.com>
2023-12-30 20:13:50 -0800

    miners

--- Game/base.java
+++ Game/base.java
@@ -4,42 +4,43 @@ import java.util.ArrayList;
 
 public class base extends actor {
     private int money;
-    private int income;
 
-    public base(int startingMoney, int startingIncome, double xpos, double ypos, ArrayList<actor> allies, ArrayList<actor> enemies, Game game) {
+    public base(int startingMoney, double xpos, double ypos, ArrayList<actor> allies, ArrayList<actor> enemies, Game game) {
         super(allies, enemies, game);
         money = startingMoney;
-        income = startingIncome;
         x = xpos;
         y = ypos;
     }
 
-    public void invest(int amount) {
-        if (amount < money) {
-            money -= amount;
-            income += amount / 100;
-        }
-    }
-
     public void spawnInfantryGuy() {
-        if (money >= 150) {
-            money -= 150;
+        if (money >= 1000) {
+            money -= 1000;
             infantryGuy infantry = new infantryGuy(x, y, allies, enemies, game);
             game.add(infantry, allies);
         }
 
     }
 
+    public void spawnMineGuy() {
+        if (money >= 500) {
+            money -= 500;
+            mineGuy miner = new mineGuy(x, y, allies, enemies, game);
+            game.add(miner, allies);
+        }
+
+    }
+
+    protected void addMoney(int cash) {
+        cash += money;
+    }
+
     public int getMoney() {
         return money;
     }
 
-    public int getIncome() {
-        return income;
-    }
-
     @Override
     public void tick() {
-        money += income;
+
+
     }
 }
